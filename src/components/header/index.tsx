20ad37ed4a267fb0408c8f2157b50e376931635e
unknown <matheusfariasdasneves@gmail.com>
2023-12-13 20:46:08 -0300

    feat: adicionado estilizaçao inicial do componente Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="w-full h-[4.7em] px-8 py-4 bg-primary" >
+      
+    </div>
+  );
+};
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,10 @@
+import Header from "../../components/header";
+
 export default function About() {
   return (
     <div>
+      <Header/>
+
       <h1>Page About</h1>
     </div>
   );
