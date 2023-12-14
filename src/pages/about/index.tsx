47edcaf591f9539700498d131711896060600880
unknown <matheusfariasdasneves@gmail.com>
2023-12-13 21:41:23 -0300

    feat: adicionado imagem de perfil na pagina about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,22 @@
 import Background from "../../components/background";
 import Header from "../../components/header";
 
+import avatarImage from "../../assets/foto-perfil.jpeg";
+
 export default function About() {
   return (
     <Background>
       <Header/>
 
-      <h1 className="text-white" >Page About</h1>
+      <div className="px-[1.2em] py-10 w-full flex flex-col items-center" >
+        <div>
+          <img
+            src={ avatarImage }
+            alt="Foto Perfil"
+            className="object-cover rounded-full w-[350px] h-[350px] object-top border-8 border-secondary "
+          />
+        </div>
+      </div>
     </Background>
   );
 };
