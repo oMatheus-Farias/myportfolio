765933d0f1960466f8dbf40583eb99276513943a
unknown <matheusfariasdasneves@gmail.com>
2023-12-13 21:15:34 -0300

    feat: adicionado a logo dentro de um componente Link para sempre que clicado pelo usuario navegar para a pagina about

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,17 @@
 import logo from "../../assets/logo.svg";
 import { FiMenu } from "react-icons/fi";
 
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <div className="w-full h-[4.7em] px-[1.2em] py-4 bg-primary flex items-center justify-between" >
-      <img
-        src={ logo }
-        alt="Matheus Farias - Desenvolvedor Web"
-      />
+      <Link to="/" >
+        <img
+          src={ logo }
+          alt="Matheus Farias - Desenvolvedor Web"
+        />
+      </Link>
 
       <button>
         <FiMenu 
