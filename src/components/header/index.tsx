56f167e26c0077aa3a157f938e1b467147fa83d6
unknown <matheusfariasdasneves@gmail.com>
2023-12-13 21:11:19 -0300

    feat: adicionado logo e icone do menu no componente Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,15 @@
+import logo from "../../assets/logo.svg";
+import { FiMenu } from "react-icons/fi";
+
 export default function Header() {
   return (
-    <div className="w-full h-[4.7em] px-8 py-4 bg-primary" >
-      
+    <div className="w-full h-[4.7em] px-[1.2em] py-4 bg-primary flex items-center justify-between" >
+      <img
+        src={ logo }
+        alt="Matheus Farias - Desenvolvedor Web"
+      />
+
+      <FiMenu color="#FFFBFF" size={36} />
     </div>
   );
 };
