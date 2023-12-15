47fc369cc30aa05b9e86c294aea762f0b331cdc0
unknown <matheusfariasdasneves@gmail.com>
2023-12-15 19:33:58 -0300

    feat: adicionado caminho no link de email e github na pagina sobre

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -55,8 +55,8 @@ export default function About() {
         
         
           <div className="w-full flex justify-start items-center gap-6 mt-5" >
-            <a href="#" title="Email" target="_blank" ><MdEmail color="#61F2C2" size={36} /></a>
-            <a href="#" title="GitHub" target="_blank" ><FaGithub color="#61F2C2" size={32} /></a>
+            <a href="mailto:matheusfariasdasneves@gmail.com" title="Email" target="_blank" ><MdEmail color="#61F2C2" size={36} /></a>
+            <a href="https://github.com/oMatheus-Farias" title="GitHub" target="_blank" ><FaGithub color="#61F2C2" size={32} /></a>
             <a href="#" title="LinkedIn" target="_blank" ><FaLinkedin color="#61F2C2" size={32} /></a>
             <button 
               title="Baixar currículo"
