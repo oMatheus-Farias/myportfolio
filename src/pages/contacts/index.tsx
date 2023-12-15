348e06c0d15365109f9e0b9aaabee42299b8ee5b
unknown <matheusfariasdasneves@gmail.com>
2023-12-15 11:11:30 -0300

    feat: adicionado estrutura inicial das paginas projetos e contatos

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,7 +1,22 @@
+import { useContext, useEffect } from "react";
+import { Context } from "../../contexts";
+
+import Background from "../../components/background";
+import Header from "../../components/header";
+import Nav from "../../components/nav";
+
 export default function Contacts() {
+  const { viewDasktop, openOrCloseMenu, setOpenOrCloseMenu } = useContext(Context);
+
+  useEffect(() => {
+    setOpenOrCloseMenu(false);
+  }, []);
+
   return (
-    <div>
-      <h1>Page Contacts</h1>
-    </div>
+    <Background>
+      <Header openMenu={ () => setOpenOrCloseMenu(!openOrCloseMenu) } viewSize={ viewDasktop } />
+
+      { openOrCloseMenu && <Nav closeMenu={ () => setOpenOrCloseMenu(!openOrCloseMenu) } /> }
+    </Background>
   );
 };
