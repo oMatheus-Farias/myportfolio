c70d95235269593b5b9fe33c8a5deb859b1235e3
unknown <matheusfariasdasneves@gmail.com>
2023-12-15 19:16:09 -0300

    feat: adicionado indicador da pagina renderizada na navegaçao dasktop

--- src/components/navDasktop/index.tsx
+++ src/components/navDasktop/index.tsx
@@ -1,27 +1,38 @@
+import { useContext } from "react";
+import { Context } from "../../contexts";
+
 import { Link } from "react-router-dom";
 
 export default function NavDasktop(){
+  const { currentPage } = useContext(Context);
+
   return(
     <nav className="flex items-center gap-8 lg:gap-16" >
       <Link 
-          className="text-textColor font-bold"
+          className="text-textColor font-bold border-b-2"
+          style={{ borderBottomColor: currentPage === '/' ? '#F24C00' : 'transparent' }}
           to='/' 
         >
           sobre
         </Link>
         <Link 
-          className="text-textColor font-bold"
+          className="text-textColor font-bold border-b-2"
+          style={{ borderBottomColor: currentPage === '/specialties' ? '#F24C00' : 'transparent' }}
           to='/specialties' 
         >
           especialidades
         </Link>
         <Link 
-          className="text-textColor font-bold"
+          className="text-textColor font-bold border-b-2"
+          style={{ borderBottomColor: currentPage === '/projects' ? '#F24C00' : 'transparent' }}
           to='/projects' 
         >
           projetos
         </Link>
-        <div className="bg-secondary rounded-xl py-1 px-2" >
+        <div 
+          className="rounded-xl py-1 px-2" 
+          style={{ backgroundColor: currentPage === '/contacts' ? '#F24C00' : '#61F2C2' }}
+        >
           <Link 
             className="text-primary font-bold"
             to='/contacts' 
