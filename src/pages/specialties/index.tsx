bccdee511991cef5cc457453837b8c53d9971e7c
unknown <matheusfariasdasneves@gmail.com>
2023-12-15 13:33:37 -0300

    ajustado display grid para diferentes tamanhos de tela

--- src/pages/specialties/index.tsx
+++ src/pages/specialties/index.tsx
@@ -21,64 +21,64 @@ export default function Specialties() {
     <Background>
       <Header openMenu={ () => setOpenOrCloseMenu(!openOrCloseMenu) } viewSize={ viewDasktop } />
 
-      <main className="px-[4em] py-9 mt-9 grid grid-cols-1 gap-5" >
-        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8" >
+      <main className="px-10 py-9 mt-6 gap-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:px-36 lg:gap-24" >
+        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
           <FaHtml5 color="#FFFBFF" size={142} />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >Html</p>
           </div>
         </section>
-        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8" >
+        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
           <FaCss3Alt color="#FFFBFF" size={142} />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >Css</p>
           </div>
         </section>
-        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8" >
+        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
           <RiJavascriptFill  color="#FFFBFF" size={142} />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >Javascript</p>
           </div>
         </section>
-        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8" >
+        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
           <BiLogoTypescript color="#FFFBFF" size={142} />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >Typescript</p>
           </div>
         </section>
-        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8" >
+        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
           <FaReact color="#FFFBFF" size={142} />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >React</p>
           </div>
         </section>
-        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8" >
+        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
           <BiLogoTailwindCss color="#FFFBFF" size={142} />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >Tailwindcss</p>
           </div>
         </section>
-        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8" >
+        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
           <FaGitAlt color="#FFFBFF" size={142} />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >Git</p>
           </div>
         </section>
-        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8" >
+        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
           <FaGithub color="#FFFBFF" size={142} />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >Github</p>
           </div>
         </section>
-        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8" >
+        <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
           <FaFigma color="#FFFBFF" size={142} />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
