2e49093acc5d8e7e62eb63adf10e845a3b8ed5d5
unknown <matheusfariasdasneves@gmail.com>
2023-12-15 10:09:42 -0300

    feat: adicionado componentes, Background, Header e Nav na pagina de especialidades

--- src/pages/specialties/index.tsx
+++ src/pages/specialties/index.tsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { Context } from "../../contexts";
+
+import Background from "../../components/background";
+import Header from "../../components/header";
+import Nav from "../../components/nav";
+
 export default function Specialties() {
+  const { viewDasktop, openOrCloseMenu, setOpenOrCloseMenu } = useContext(Context);
+
   return (
-    <div>
-      <h1>Page Specialties</h1>
-    </div>
+    <Background>
+      <Header openMenu={ () => setOpenOrCloseMenu(!openOrCloseMenu) } viewSize={ viewDasktop } />
+
+      { openOrCloseMenu && <Nav closeMenu={ () => setOpenOrCloseMenu(!openOrCloseMenu) } /> }
+    </Background>
   );
 };
