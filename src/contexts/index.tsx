9f65d09da710727c177005ee4f821cbabb04387e
unknown <matheusfariasdasneves@gmail.com>
2023-12-16 12:53:42 -0300

    feat: adicionado array de objetos technologies que armazena todas as tecnologias que serao renderizadas na pagina de especialidades

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,11 +1,27 @@
 import { createContext, ReactNode, useState, useEffect } from "react";
 
+import htmlImage from "../assets/html.png";
+import cssImage from "../assets/css.png";
+import javascriptImage from "../assets/javascript.png";
+import typescriptImage from "../assets/typescript.png";
+import reactImage from "../assets/react.png";
+import tailwindcssImage from "../assets/tailwindcss.png";
+import gitImage from "../assets/git.png";
+import githubImage from "../assets/github.png";
+import figmaImage from "../assets/figma.png";
+
 interface ContextData {
   viewDasktop: boolean,
   openOrCloseMenu: boolean,
   setOpenOrCloseMenu: (openOrCloseMenu: boolean) => void;
   currentPage: string,
-  setCurrentPage: any
+  setCurrentPage: any,
+  technologies: technologiesData[]
+};
+
+type technologiesData = {
+  image: string,
+  name: string
 };
 
 export const Context = createContext({} as ContextData);
@@ -14,6 +30,45 @@ export default function Provider({ children }: { children: ReactNode }){
   const [viewDasktop, setViewDascktop] = useState<any>(null);
   const [openOrCloseMenu, setOpenOrCloseMenu] = useState(false);
   const [currentPage, setCurrentPage] = useState('');
+
+  const technologies = [
+    {
+      image: htmlImage,
+      name: 'Html'
+    },
+    {
+      image: cssImage,
+      name: 'Css'
+    },
+    {
+      image: javascriptImage,
+      name: 'Javascript'
+    },
+    {
+      image: typescriptImage,
+      name: 'Typescripy'
+    },
+    {
+      image: reactImage,
+      name: 'React'
+    },
+    {
+      image: tailwindcssImage,
+      name: 'Tailwindcss'
+    },
+    {
+      image: gitImage,
+      name: 'Git'
+    },
+    {
+      image: githubImage,
+      name: 'Github'
+    },
+    {
+      image: figmaImage,
+      name: 'Figma'
+    },
+  ];
 
   useEffect(() => {
     handleViewSize();
@@ -29,7 +84,7 @@ export default function Provider({ children }: { children: ReactNode }){
   };
 
   return(
-    <Context.Provider value={{ viewDasktop, openOrCloseMenu, setOpenOrCloseMenu, currentPage, setCurrentPage }} >
+    <Context.Provider value={{ viewDasktop, openOrCloseMenu, setOpenOrCloseMenu, currentPage, setCurrentPage, technologies }} >
       { children }
     </Context.Provider>
   );
--- src/pages/specialties/index.tsx
+++ src/pages/specialties/index.tsx
@@ -10,6 +10,9 @@ import { RiJavascriptFill } from "react-icons/ri";
 import { BiLogoTypescript, BiLogoTailwindCss } from "react-icons/bi";
 import { FaGitAlt } from "react-icons/fa6";
 
+import htmlImage from "../../assets/html.png";
+import cssImage from "../../assets/css.png";
+
 export default function Specialties() {
   const { viewDasktop, openOrCloseMenu, setOpenOrCloseMenu, setCurrentPage } = useContext(Context);
 
@@ -28,14 +31,18 @@ export default function Specialties() {
 
       <main className="px-10 py-9 mt-6 gap-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:px-36 lg:gap-24" >
         <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
-          <FaHtml5 color="#FFFBFF" size={142} />
+          <img
+            src={htmlImage}
+          />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >Html</p>
           </div>
         </section>
         <section className="bg-cardsColor rounded-xl flex flex-col items-center pt-8 max-w-md mx-auto min-w-[15em]" >
-          <FaCss3Alt color="#FFFBFF" size={142} />
+          <img
+            src={cssImage}
+          />
 
           <div className="bg-primary w-full py-2 text-center rounded-b-xl mt-8" >
             <p className="text-secondary uppercase" >Css</p>
